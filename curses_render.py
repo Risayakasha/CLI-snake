aa809076713f8bbb0267086a301744f8b77e2fdb
skunk <dimabulka8@gmail.com>
2026-02-20 15:44:02 +0200

    Polish

--- curses_render.py
+++ curses_render.py
@@ -2,6 +2,7 @@ from time import sleep
 import curses
 from curses import wrapper
 from snake_engine import SnakeSegment, Food
+from collections import deque
 
 stdscr = curses.initscr()
 curses.noecho()
@@ -9,7 +10,6 @@ curses.cbreak()
 stdscr.notimeout(True)
 stdscr.keypad(True)
 stdscr.nodelay(True)
-
 curses.start_color()
 curses.use_default_colors()
 curses.curs_set(0)
@@ -19,53 +19,57 @@ height, width = stdscr.getmaxyx()
 centery, centerx = height // 2, width // 2
 
 
+def GameOver(s):
+    s.clear()
+    s.addstr(centery, centerx - 4, "GAY OVER")
+    s.nodelay(False)
+    s.getkey()
+
+
 def main(stdscr):
     food = Food(width, height)
-    head = SnakeSegment(centerx, centery)
-    body = [head]
+    snake = SnakeSegment(centerx, centery, [0, 0, width, height])
 
-    food_count = 0
     game = True
+    tail = None
 
     food.spawnfood()
-    food_count += 1
 
     while game is True:
+        head = [snake.x, snake.y]
         key = stdscr.getch()
-        stdscr.clear()
+        stdscr.erase()
 
         stdscr.addstr(food.y, food.x, "󰉛")
 
-        if head.x == 0 or head.x == width or head.y == 0 or head.y == height:
-            stdscr.clear()
-            stdscr.addstr(centery, centerx - 4, "GAME OVER")
-            stdscr.nodelay(False)
+        stdscr.addstr(head[1], head[0], "")
+
+        if len(snake.body) > 0:
+            n = 0
+            while n < len(snake.body):
+                stdscr.addstr(snake.body[n][1], snake.body[n][0], "")
+                n += 1
+
+            tail = snake.body[-1]
+
+        if snake.colision() is True:
             game = False
-            stdscr.getkey()
+            GameOver(stdscr)
+
+        stdscr.noutrefresh()
+
+        snake.move()
+
+        if head[0] == food.x and head[1] == food.y:
+            snake.grow(tail)
+            food.spawnfood()
 
         if key == 260:
-            head.turn(1)
+            snake.turn(1)
         if key == 261:
-            head.turn(0)
+            snake.turn(0)
 
-        if head.x == food.x and head.y == food.y:
-            body.append(body[-1].grow())
-            food_count = 0
-            food.spawnfood()
-            food_count += 1
-        stdscr.addstr(head.y, head.x, "")
-
-        n = 0
-        for n in range(len(body)):
-            # stdscr.addstr(0, 0, str(n))
-            if n == 0:
-                continue
-            else:
-                stdscr.addstr(body[n].y, body[n].x, "")
-                body[n].move(body[n - 1].d)
-        head.move(head.d)
-
-        stdscr.refresh()
+        curses.doupdate()
         sleep(0.1)
 
 
--- snake_engine.py
+++ snake_engine.py
@@ -1,40 +1,34 @@
+from collections import deque
 import random as r
-
-# class Grid:
-#   def __init__(self, width: int, height: int) -> None:
-#      self.width = width
-#     self.height = height
-#        self.plane = [[0 for r in range(width)] for c in range(height)]
-#
-#   def updateScr(self, snakeX, snakeY):
-#
-#        self.plane[snakeY][snakeX] = 8
-#       for i in range(self.height):
-#          print(self.plane[i])
-#     print("")
 
 
 class SnakeSegment:
 
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int, bounds: list):
         self.x = x
         self.y = y
+
+        self.bounds = bounds
 
         self.dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
         self.d = 0  # up, right, down, left
 
-    def move(self, d):
-        delta = self.dir[d]
+        self.previous = []
+        self.body = deque([])
 
-        self.previous_coords = [self.x, self.y]
+    def move(self):
+        delta = self.dir[self.d]
+        self.previous = [self.x, self.y]
 
         self.x += delta[0]
         self.y -= delta[1]
 
-    def turn(self, key):
-        self.previous_dir = self.d
-        # if loop for input
+        if len(self.body) > 0:
+            self.body.pop()
+            self.body.appendleft(self.previous)
+
+    def turn(self, key: int):
 
         if key == 0:
             self.d += 1
@@ -42,11 +36,23 @@ class SnakeSegment:
             self.d -= 1
         self.d %= 4
 
-    def grow(self):
-        x, y = self.previous_coords[0], self.previous_coords[1]
+    def grow(self, last_seg):
+        if last_seg is not None:
+            x, y = last_seg[0], last_seg[1]
+        else:
+            x, y = self.previous[0], self.previous[1]
 
-        segment = SnakeSegment(x, y)
-        return segment
+        self.body.append([x, y])
+
+    def colision(self):
+        if self.x not in range(self.bounds[0], self.bounds[2]) or self.y not in range(
+            self.bounds[1], self.bounds[3]
+        ):
+            return True
+        if [self.x, self.y] in self.body:
+            return True
+        else:
+            return False
 
 
 class Food:
@@ -55,5 +61,5 @@ class Food:
         self.max_y = max_y
 
     def spawnfood(self):
-        self.x = r.randint(0, self.max_x)
-        self.y = r.randint(0, self.max_y)
+        self.x = r.randint(5, self.max_x - 5)
+        self.y = r.randint(5, self.max_y - 5)
